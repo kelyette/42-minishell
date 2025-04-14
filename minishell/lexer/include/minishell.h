6fca2719fffbc7a0a7af64be9b9d93e9c173bda6
Nguyen NGUYEN <154276946+TheOrangeCake@users.noreply.github.com>
2025-04-14 11:25:57 +0200

    Add files via upload

---
+++ minishell/lexer/include/minishell.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hoannguy <hoannguy@student.42lausanne.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/24 15:15:04 by kcsajka           #+#    #+#             */
+/*   Updated: 2025/04/05 21:28:18 by hoannguy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+# include <stdio.h>
+# include <stdlib.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+
+#endif
