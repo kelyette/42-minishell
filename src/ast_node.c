d5f59cd0bbce035748ff3c14db6fffa971f7953b
kelyette <66877262+kelyette@users.noreply.github.com>
2025-04-19 21:21:46 +0200

    update ast_node.c

--- src/ast_node.c
+++ src/ast_node.c
@@ -6,7 +6,7 @@
 /*   By: kcsajka <kcsajka@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/05 15:24:55 by kcsajka           #+#    #+#             */
-/*   Updated: 2025/04/08 05:06:16 by kcsajka          ###   ########.fr       */
+/*   Updated: 2025/04/10 16:48:36 by kcsajka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ t_node	*create_node(int type)
 	t_node	*node;
 
 	node = calloc(1, sizeof(t_node)); // TODO replace forbidden func
+	if (!node)
+		return (perror("minishell: "), NULL);
 	node->type = type;
 	return (node);
 }
@@ -26,16 +28,21 @@ t_node	*create_parent(int type, t_node *lchild, t_node *rchild)
 	t_node	*node;
 
 	node = create_node(type);
+	if (!node)
+		return (free_tree(&lchild), free_tree(&rchild), NULL);
 	node->lnode = lchild;
 	node->rnode = rchild;
 	return (node);
 }
 
-void	free_tree(t_node *root)
+void	free_tree(t_node **rootptr)
 {
-	if (root->lnode)
-		free_tree(root->lnode);
-	if (root->rnode)
-		free_tree(root->rnode);
-	free(root);
+	if (!*rootptr)
+		return ;
+	if ((*rootptr)->lnode)
+		free_tree(&(*rootptr)->lnode);
+	if ((*rootptr)->rnode)
+		free_tree(&(*rootptr)->rnode);
+	free(*rootptr);
+	*rootptr = NULL;
 }
