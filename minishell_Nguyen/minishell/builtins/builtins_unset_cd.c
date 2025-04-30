5d3986c9970659b8bcbb87fed2427ed9bce0cb36
Nguyen NGUYEN <154276946+TheOrangeCake@users.noreply.github.com>
2025-04-30 17:58:20 +0200

    Add files via upload

---
+++ minishell_Nguyen/minishell/builtins/builtins_unset_cd.c
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtins_unset_cd.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hoannguy <hoannguy@student.42lausanne.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/30 16:59:30 by hoannguy          #+#    #+#             */
+/*   Updated: 2025/04/30 17:56:42 by hoannguy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "builtins.h"
+
+int	builtin_cd(t_node *node)
+{
+	(void)node;
+	printf("placeholder\n");
+	return (0);
+}
+
+void	pop_env(t_env *temp, t_env **env)
+{
+	t_env	*tmp;
+
+	tmp = *env;
+	while (tmp != NULL)
+	{
+		if (tmp->next == temp)
+		{
+			tmp->next = temp->next;
+			ft_lstdelone_env(temp);
+			return ;
+		}
+		tmp = tmp->next;
+	}
+}
+
+// case unset, pop multiple variable
+int	builtin_unset(t_node *node, t_env **env)
+{
+	t_env	*temp;
+
+	if (env == NULL || *env == NULL || node->data == NULL)
+		return (0);
+	node->data = node->data->next;
+	while (node->data != NULL)
+	{
+		temp = *env;
+		while (temp != NULL)
+		{
+			if (!ft_strncmp(node->data->str, temp->key,
+					ft_compare(node->data->str, temp->key)))
+			{
+				pop_env(temp, env);
+				break ;
+			}
+			temp = temp->next;
+		}
+		node->data = node->data->next;
+	}
+	builtin_env(env);
+	return (0);
+}
