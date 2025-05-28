24adaa9256a573553003ebdd65ef872b9201d3de
TheOrangeCake <vietfox94@gmail.com>
2025-05-28 17:15:45 +0200

    signaux

--- src/core/signal.c
+++ src/core/signal.c
@@ -6,7 +6,7 @@
 /*   By: hoannguy <hoannguy@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 14:34:42 by hoannguy          #+#    #+#             */
-/*   Updated: 2025/05/28 17:08:21 by hoannguy         ###   ########.fr       */
+/*   Updated: 2025/05/28 17:15:15 by hoannguy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,8 +33,6 @@ void restore_sigint_handler(void)
     sigaction(SIGINT, &sa, NULL);
 }
 
-// change signal type
-// code 131 = code 128 +  3 (SIGQUIT)
 void	signal_change(int signal)
 {
 	if (signal == SIGINT)
@@ -45,11 +43,6 @@ void	signal_change(int signal)
 		rl_redisplay();
 		signal = 0;
 	}
-	// else if (signal == SIGQUIT) //to update to clean up child process
-	// {
-	// 	printf("Quit (Core dumped)\n");
-	// 	exit (131);
-	// }
 }
 
 int	signal_handler(void)
@@ -63,8 +56,6 @@ int	signal_handler(void)
 		return (perror("Error"), 1);
 	if (isatty(STDIN_FILENO))
 		sa.sa_handler = SIG_IGN;
-	else
-		sa.sa_handler = signal_change;
 	if (sigaction(SIGQUIT, &sa, NULL) != 0)
 		return (perror("Error"), 1);
 	return (0);
